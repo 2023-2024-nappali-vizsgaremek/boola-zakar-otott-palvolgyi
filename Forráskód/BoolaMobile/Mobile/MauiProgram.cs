c4ad8668d65767e83431872c7b6dbeb0fff149a4
Ótott-Kovács Tamás <88544068+szt2otokov0@users.noreply.github.com>
2023-12-06 13:21:19 +0100

    some files and packaging

--- Forráskód/BoolaMobile/Mobile/MauiProgram.cs
+++ Forráskód/BoolaMobile/Mobile/MauiProgram.cs
@@ -11,8 +11,7 @@ namespace Mobile
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Inter.ttf", "Inter");
                 });
 
 #if DEBUG
