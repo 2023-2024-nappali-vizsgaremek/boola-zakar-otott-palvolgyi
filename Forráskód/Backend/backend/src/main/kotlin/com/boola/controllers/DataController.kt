da726cc8cc35212fe4fe936b54d607d6bf0c9397
Ótott-Kovács Tamás <88544068+szt2otokov0@users.noreply.github.com>
2024-01-18 11:27:08 +0100

    renumbered column indices + auth db side

--- Forráskód/Backend/backend/src/main/kotlin/com/boola/controllers/DataController.kt
+++ Forráskód/Backend/backend/src/main/kotlin/com/boola/controllers/DataController.kt
@@ -4,9 +4,12 @@ import com.boola.models.Account
 import com.boola.models.Currency
 import com.boola.models.ExpenseList
 import com.boola.models.Profile
+import java.security.MessageDigest
 import java.sql.Connection
 import java.sql.PreparedStatement
 import java.util.UUID
+import kotlin.random.Random
+import kotlin.text.StringBuilder
 
 class DataController(private val connection: Connection) {
 
@@ -14,11 +17,13 @@ class DataController(private val connection: Connection) {
         "SELECT * FROM account WHERE email= ?")
     private val getAccountsStatement:PreparedStatement = connection.prepareStatement("SELECT * FROM account")
     private val addAccountStatement:PreparedStatement = connection.prepareStatement(
-        "INSERT INTO account VALUE (?,?,?)")
+        "INSERT INTO account VALUE (?,?,?,?)")
     private val setAccountStatement:PreparedStatement = connection.prepareStatement(
         "UPDATE account SET email=?, password=?,name=? WHERE email=?")
     private val deleteAccountStatement:PreparedStatement=connection.prepareStatement(
         " DELETE FROM account WHERE emial=?")
+    private val getAccountSaltStatement:PreparedStatement=connection.prepareStatement(
+        "SELECT salt from account WHERE email=?")
     private val getCurrencyStatement:PreparedStatement = connection.prepareStatement(
         "SELECT name from currency WHERE code = ?")
     private val getCurrenciesStatement:PreparedStatement = connection.prepareStatement("SELECT * FROM currency")
@@ -31,7 +36,7 @@ class DataController(private val connection: Connection) {
     private val getProfilesStatement:PreparedStatement=connection.prepareStatement("SELECT * FROM  profile")
     private val addProfileStatement:PreparedStatement=connection.prepareStatement(
         "INSERT INTO profile VAlUE (?,?,?,?,?,?,?)")
-    private val setPofileStatement:PreparedStatement=connection.prepareStatement(
+    private val setProfileStatement:PreparedStatement=connection.prepareStatement(
         "UPDATE  profile SET id=?,name=?,isBusiness=?,languageId=?,expenseListId=?,accountEmail=? WHERE id=?")
     private val deleteProfileStatement:PreparedStatement=connection.prepareStatement(
         "DELETE From profile where id=?")
@@ -44,7 +49,6 @@ class DataController(private val connection: Connection) {
         getAccountStatement.setString(1, email)
         getAccountStatement.execute()
         val results = getAccountStatement.resultSet
-        results.first()
         return Account(results.getString(0), results.getString(1),
             results.getString(2))
     }
@@ -52,47 +56,62 @@ class DataController(private val connection: Connection) {
     fun getAccountsAll():ArrayList<Account>{
         getAccountsStatement.execute()
         val results = getAccountsStatement.resultSet
-        results.first()
         val accounts = ArrayList<Account>()
-        do {
-            accounts.add(Account(results.getString(0), results.getString(1),
-                results.getString(2)))
-        } while (results.next())
+        while (results.next()) {
+            accounts.add(Account(results.getString(1), results.getString(2),
+                results.getString(3)))
+        }
         return accounts
     }
 
     fun addAccount(accountToAdd:Account){
         addAccountStatement.run {
-            setString(0,accountToAdd.email)
-            setString(1,accountToAdd.pwHash)    //todo: check model value order and name
-            setString(2,accountToAdd.name)
+            setString(1,accountToAdd.email)
+            val salt = StringBuilder()
+            for(i in 0..8){
+                salt.append(Char(Random.nextInt()))
+            }
+            val hashedPwBytes = MessageDigest.getInstance("SHA-256").digest((salt.append(accountToAdd.pwHash)
+                .toString().toByteArray()))
+            val hashedPwString:StringBuilder = StringBuilder()
+            for(b in hashedPwBytes){
+                hashedPwString.append(Char(b.toInt()))
+            }
+            setString(2,hashedPwString.toString())
+            setString(3,accountToAdd.name)
+            setString(4,salt.toString())
             execute()
         }
     }
     fun deleteAccount(accountToAdd:Account){
         deleteAccountStatement.run {
-            setString(0,accountToAdd.email)
-            setString(1,accountToAdd.pwHash)
-            setString(2,accountToAdd.name)
+            setString(1,accountToAdd.email)
+            setString(2,accountToAdd.pwHash)
+            setString(3,accountToAdd.name)
             execute()
         }
     }
 
     fun setAccount(accountEmail:String,newData:Account){
         setAccountStatement.run {
-            setString(0,newData.email)
-            setString(1,newData.pwHash)
-            setString(2,newData.name)
-            setString(3,accountEmail)
+            setString(1,newData.email)
+            setString(2,newData.pwHash)
+            setString(3,newData.name)
+            setString(4,accountEmail)
             execute()
         }
+    }
+
+    fun getAccountSalt(accountEmail: String): String {
+        getAccountSaltStatement.setString(1, accountEmail)
+        getAccountSaltStatement.execute()
+        return getAccountSaltStatement.resultSet.getString(1)
     }
 
     fun getCurrency(code:String):String {
         getCurrencyStatement.setString(1,code)
         getCurrencyStatement.execute()
         val results = getCurrencyStatement.resultSet
-        results.first()
         return results.getString(1)
     }
 
@@ -109,48 +128,54 @@ class DataController(private val connection: Connection) {
         getProfileStatement.setObject(1,id)
         getProfileStatement.execute()
         val results=getProfileStatement.resultSet
-        return Profile( UUID.fromString(results.getString(0)),results.getString(1),results.getBoolean(2),results.getString(3),UUID.fromString(results.getString(4)),results.getString(5))
+        return Profile( UUID.fromString(results.getString(1)),results.getString(2),
+            results.getBoolean(3),results.getString(4),
+            UUID.fromString(results.getString(5)),results.getString(6))
     }
-    fun getAllprofile():ArrayList<Profile>{
+    fun getAllProfile():ArrayList<Profile>{
         getProfilesStatement.execute()
         val Profiles=ArrayList<Profile>()
         val results=getProfilesStatement.resultSet
         while (results.next()){
-            Profiles.add(Profile(UUID.fromString(results.getString("id")),results.getString("name"),results.getBoolean("isBusiness"),results.getString("languageId"),UUID.fromString(results.getString("expenseListId")),results.getString("accountEmail")))
+            Profiles.add(Profile(UUID.fromString(results.getString("id")),
+                results.getString("name"),results.getBoolean("isBusiness"),
+                results.getString("languageId"),
+                UUID.fromString(results.getString("expenseListId")),
+                results.getString("accountEmail")))
         }
         return Profiles
     }
     fun setProfile(id:UUID,newData:Profile){
-        setPofileStatement.run {
-            setObject(0,newData.id)
-            setString(1,newData.name)
-            setBoolean(2,newData.isBusiness)
-            setString(3,newData.languageId)
-            setObject(4,newData.expenseListId)
-            setString(5,newData.accountEmail)
-            setObject(6,id)
+        setProfileStatement.run {
+            setObject(1,newData.id)
+            setString(2,newData.name)
+            setBoolean(3,newData.isBusiness)
+            setString(4,newData.languageId)
+            setObject(5,newData.expenseListId)
+            setString(6,newData.accountEmail)
+            setObject(7,id)
             execute()
         }
     }
     fun deleteProfile(newData: Profile){
         deleteProfileStatement.run {
-            setObject(0,newData.id)
-            setString(1,newData.name)
-            setBoolean(2,newData.isBusiness)
-            setString(3,newData.languageId)
-            setObject(4,newData.expenseListId)
-            setString(5,newData.accountEmail)
+            setObject(1,newData.id)
+            setString(2,newData.name)
+            setBoolean(3,newData.isBusiness)
+            setString(4,newData.languageId)
+            setObject(5,newData.expenseListId)
+            setString(6,newData.accountEmail)
             execute()
         }
     }
     fun addProfile(newData:Profile){
         addProfileStatement.run {
-            setObject(0,newData.id)
-            setString(1,newData.name)
-            setBoolean(2,newData.isBusiness)
-            setString(3,newData.languageId)
-            setObject(4,newData.expenseListId)
-            setString(5,newData.accountEmail)
+            setObject(1,newData.id)
+            setString(2,newData.name)
+            setBoolean(3,newData.isBusiness)
+            setString(4,newData.languageId)
+            setObject(5,newData.expenseListId)
+            setString(6,newData.accountEmail)
             execute()
         }
     }
@@ -158,27 +183,25 @@ class DataController(private val connection: Connection) {
 
 
     fun getExpenseList(id: UUID):ExpenseList{
-        getExpenseListStatement.setObject(0,id)
+        getExpenseListStatement.setObject(1,id)
         getExpenseListStatement.execute()
 
-        val results = getExpenseListStatement.resultSet;
-        results.first()
+        val results = getExpenseListStatement.resultSet
         return ExpenseList(
-            UUID.fromString(results.getString(0)),results.getLong(1),
-            results.getString(2))
+            UUID.fromString(results.getString(1)),results.getLong(2),
+            results.getString(3))
     }
 
     fun getExpenseListsAll():ArrayList<ExpenseList>{
         getExpenseListsStatement.execute()
         val lists = ArrayList<ExpenseList>()
         val results = getExpenseListsStatement.resultSet
-        results.first()
-        do {
+        while(results.next()) {
             lists.add(ExpenseList(
-                UUID.fromString(results.getString(0)),results.getLong(1),
-                results.getString(2)))
+                UUID.fromString(results.getString(1)),results.getLong(2),
+                results.getString(3)))
             results.next()
-        } while (!results.isLast)
+        }
         return lists
     }
 
