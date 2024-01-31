1f4ca3ca0b59b6220bec8c217dfa4b0e217020dc
patrik7776 <discoorddiscord60@gmail.com>
2024-01-31 12:19:29 +0100

    bugs fixedd

---
+++ Forráskód/BoolaDesktop/BoolaShared/Models/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desktop.Models
+{
+    public class Account
+    {
+        public string email { get; set; }
+        public string pwHash { get; set; }
+        public string name { get; set; }
+
+        public Account(string email, string pwHash, string name)
+        {
+            this.email = email;
+            this.pwHash = pwHash;
+            this.name = name;
+        }
+    }
+}
---
+++ Forráskód/BoolaDesktop/BoolaShared/Models/MainContent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desktop.Models
+{
+    public record MainContent(string WelcomeText, double SpendPercent, double SpendDelta, double MonthlySpend,
+                                double Balance)
+    {
+        internal MainContent() : this("Welcome", 0, 0, 0, 69)
+        {
+        }
+    }
+}
