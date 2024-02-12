4d7d8bd3699c94937552273155d31006b5bf1164
Z4kB4l <szt2zakbal@vasvari.org>
2024-02-12 21:11:11 +0100

    vue web alap

---
+++ Forráskód/BoolaWeb/vue-boola/src/router/index.js
@@ -0,0 +1,41 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import basePage from "@/views/basePage"
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: basePage
+    },
+    {
+      path: '/newExpense',
+      name: 'newExpense',      
+      component: () => import('../views/newExpense.vue')
+    },
+    {
+      path: '/profileSelect',
+      name: 'profileSelect',      
+      component: () => import('../views/profileSelect.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',      
+      component: () => import('../views/login.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',      
+      component: () => import('../views/register.vue')
+    },
+    {
+      path: '/settings',
+      name: 'settings',      
+      component: () => import('../views/settings.vue')
+    },
+  ]
+})
+
+export default router
