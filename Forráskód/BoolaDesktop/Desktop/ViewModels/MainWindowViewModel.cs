0e760e90b143db82da8579245204e664f8cde38f
Ótott-Kovács Tamás <szt2otokov@vasvari.org>
2024-02-19 13:26:01 +0100

    merge conflict resolved again

--- Forráskód/BoolaDesktop/Desktop/ViewModels/MainWindowViewModel.cs
+++ Forráskód/BoolaDesktop/Desktop/ViewModels/MainWindowViewModel.cs
@@ -13,18 +13,6 @@ namespace Desktop.ViewModels
 {
     public partial class MainWindowViewModel : BoolaShared.ViewModels.MainWindowViewModel
     {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-
-        private ObservableObject childViewModel;
-        private NewExpenseViewModel newExpenseViewModel;
-        private ProfileViewModel profileViewModel;
-        private SettingsViewModel settingsViewModel;
-
-        public static MainWindowViewModel Instance { get; set; }
-
-
-=======
         [ObservableProperty]
         private ObservableObject childViewModel;
         private NewExpenseViewModel newExpenseViewModel;
@@ -33,19 +21,6 @@ namespace Desktop.ViewModels
 
         public static MainWindowViewModel Instance { get; set; }
 
-
->>>>>>> Stashed changes
-=======
-        [ObservableProperty]
-        private ObservableObject childViewModel;
-        private NewExpenseViewModel newExpenseViewModel;
-        private ProfileViewModel profileViewModel;
-        private SettingsViewModel settingsViewModel;
-
-        public static MainWindowViewModel Instance { get; set; }
-
-
->>>>>>> Stashed changes
         public MainWindowViewModel()
         {
 
@@ -63,8 +38,6 @@ namespace Desktop.ViewModels
 
         public MainWindowViewModel(LoginViewModelDesktop childViewModel, NewExpenseViewModel newExpenseViewModel,
             ProfileViewModel profileViewModel, SettingsViewModel settingsViewModel)
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
         {
 
             this.childViewModel = childViewModel;
@@ -78,77 +51,24 @@ namespace Desktop.ViewModels
         public void ChangeToAddWindow()
         {
             childViewModel = newExpenseViewModel;
-=======
-        {
-=======
-        {
->>>>>>> Stashed changes
-
-            this.childViewModel = childViewModel;
-            Instance ??= this;
-            this.newExpenseViewModel = newExpenseViewModel;
-            this.profileViewModel = profileViewModel;
-            this.settingsViewModel = settingsViewModel;
         }
-
-        [RelayCommand]
-        public new void ChangeToAddWindow()
-        {
-            ChildViewModel = newExpenseViewModel;
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-        }
-
-
 
         [RelayCommand]
         public new void ChangeToSettingsWindow()
         {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
             childViewModel = settingsViewModel;
         }
 
         [RelayCommand]
-        public void ChangeToProfilesWindow()
-        {
-            childViewModel = profileViewModel;
-=======
-            ChildViewModel = settingsViewModel;
-        }
-
-        [RelayCommand]
         public new void ChangeToProfilesWindow()
         {
             ChildViewModel = profileViewModel;
->>>>>>> Stashed changes
         }
 
         [RelayCommand]
         public override void ChangeToMainWindow()
         {
-<<<<<<< Updated upstream
             childViewModel = new MainMenuViewModel();
         }
     }
 }
-=======
-            ChildViewModel = new Mai
->>>>>>> Stashed changes
-=======
-            ChildViewModel = settingsViewModel;
-        }
-
-        [RelayCommand]
-        public new void ChangeToProfilesWindow()
-        {
-            ChildViewModel = profileViewModel;
-        }
-
-        [RelayCommand]
-        public override void ChangeToMainWindow()
-        {
-            ChildViewModel = new Mai
->>>>>>> Stashed changes
