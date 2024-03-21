6007d51164371b7b0459a1064b36b9ac99dc543f
Ótott-Kovács Tamás <szt2otokov@vasvari.org>
2024-03-21 13:32:12 +0100

    debug progress on going

---
+++ Forráskód/BoolaWeb/vue-boola/src/views/ExpenseContent.vue
@@ -0,0 +1,14 @@
+<script setup>
+import axios from "axios";
+import {ref} from "vue";
+
+
+
+</script>
+<template>
+<h1>Költségek</h1>
+
+</template>
+<style setup>
+
+</style>
---
+++ Forráskód/BoolaWeb/vue-boola/src/views/ExpenseList.vue
@@ -0,0 +1,14 @@
+<script setup>
+import axios from "axios";
+import {ref} from "vue";
+import expense from "ExpenseContent"
+
+
+
+</script>
+<template>
+<h1>Költségek</h1>
+<expense></expense>
+</template>
+<style setup>
+</style>
