9fa81a74d6bd52e54d64f2e85dfcb00dfd27f338
szt2palpat <144673180+szt2palpat@users.noreply.github.com>
2024-04-15 14:43:50 +0200

    added expenseList PUT, DELETE APIs and trying to make profile adding work

--- Forráskód/BoolaWeb/vue-boola/src/views/Profiles.vue
+++ Forráskód/BoolaWeb/vue-boola/src/views/Profiles.vue
@@ -1,8 +1,8 @@
 <script setup>
 import axios from 'axios';
-import { onMounted, ref } from 'vue';
-import { v4 as uuidv4 } from "uuid";
-import { profileStore } from "/src/stores/ProfileStore"
+import {onMounted, ref} from 'vue';
+import {v4 as uuidv4} from "uuid";
+import {profileStore} from "/src/stores/ProfileStore"
 
 const authToken = sessionStorage.getItem("authToken");
 if (!authToken) window.open("/login", "_self")
@@ -10,17 +10,23 @@ if (!authToken) window.open("/login", "_self")
 const store = profileStore()
 const hostName = "boola-backend-a71954a87e5d.herokuapp.com"
 const profileCreation = ref(false);
-const newProfile = ref({ id: uuidv4(), name: null, isBusiness: false, languageId: null, expenseListId: null, accountEmail: store.email })
-
+const newProfile = ref({
+  id: uuidv4(),
+  name: null,
+  isBusiness: false,
+  languageId: null,
+  expenseListId: null,
+  accountEmail: store.email
+})
 const profileCreationToggle = () => {
-    profileCreation.value = !profileCreation.value;
+  profileCreation.value = !profileCreation.value;
 }
 
 const profiles = ref();
-axios.get(`http://${hostName}/api/profile`,{
-    headers: {
-        Authorization: `Bearer ${authToken}`
-    }
+axios.get(`http://${hostName}/api/profile`, {
+  headers: {
+    Authorization: `Bearer ${authToken}`
+  }
 })
     .then(r => profiles.value = r.data);
 
@@ -33,129 +39,152 @@ const languages = ref([]);
 const selectedLanguage = ref(null)
 axios.get(`http://${hostName}/api/language`)
     .then(r => languages.value = r.data);
-
+const currencies = ref([])
+const selectedCurrency = ref(null)
+axios.get(`https://${hostName}/api/currency`)
+    .then(r => currencies.value = r.data)
 const createNewProfile = () => {
-    newProfile.value.languageId = selectedLanguage.value;
-    axios.post(`http://${hostName}/api/profile`, newProfile.value, {
-        headers: {
-            Authorization: `Bearer ${authToken}`
+  newProfile.value.languageId = selectedLanguage.value;
+  axios.post(`http://${hostName}/api/profile`, newProfile.value, {
+    headers: {
+      Authorization: `Bearer ${authToken}`
+    }
+  })
+      .then(r => {
+        if (r.status != 201) {
+          alert("Hiba történt!")
+          return;
         }
-    })
-        .then(r => {
-            if (r.status != 201) {
-                alert("Hiba történt!")
-                return;
-            }
-            const profileAddress = r.data
-            axios.get(`https://${hostName}/${profileAddress}`, {
-                Authorization: `Bearer ${authToken}`
-            }).then(p => {
-                if (p.status != 200) {
-                    alert("Lekérdezési hiba történt!")
-                    return;
-                }
-                store.$patch({
-                    email:store.email,
-                    profile: p.data
-                })
-            })
-        });
+        const profileAddress = r.data
+        axios.get(`https://${hostName}/${profileAddress}`, {
+          headers: {
+            Authorization: `Bearer ${authToken}`
+          }
+        }).then(p => {
+          if (p.status != 200) {
+            alert("Lekérdezési hiba történt!")
+            return;
+          }
+          axios.put(`https://${hostName}/api/expenselist`, {
+            id: p.data.expenseListId,
+            balance: 0,
+            currencyCode: selectedCurrency.value
+          })
+          store.$patch({
+            email: store.email,
+            profile: p.data
+          })
+        })
+      });
 
-    console.log(newProfile.value);
+  console.log(newProfile.value);
 
-    newProfile.value = { id: uuidv4(), name: null, isBusiness: null, languageId: null, expenseListId: null, accountEmail: store.email };
+  newProfile.value = {
+    id: uuidv4(),
+    name: null,
+    isBusiness: null,
+    languageId: null,
+    expenseListId: null,
+    accountEmail: store.email
+  };
 }
 
 const SelectProfile = (profile) => {
-    store.$patch({
-        email:store.email,
-        profile:profile
-    })
-    window.open("/","_self")
+  store.$patch({
+    email: store.email,
+    profile: profile
+  })
+  window.open("/", "_self")
 }
 </script>
 
 <template>
-    <h1 class="text-center">Profilok</h1>
-    <div class="profiles-container" v-if="!profileCreation">
-        <div class="profile-container" v-for="p in profiles" @click="SelectProfile(p)">
-            <h2>{{ p.name }}</h2>
-            {{ p.accountEmail }}
-        </div>
-        <div @click="profileCreationToggle" class="new-profile profile-container"><span
-                class="material-symbols-outlined size-32">add</span></div>
+  <h1 class="text-center">Profilok</h1>
+  <div class="profiles-container" v-if="!profileCreation">
+    <div class="profile-container" v-for="p in profiles" @click="SelectProfile(p)">
+      <h2>{{ p.name }}</h2>
+      {{ p.accountEmail }}
     </div>
-    <div v-if="profileCreation" class="profileCreation-container">
-        <div class="profileCreationForm">
-            <h2>Név: </h2>
-            <input type="text" v-model="newProfile.name">
-        </div>
-        <div class="profileCreationForm">
-            <h2>Munkai-e: </h2>
-            <input type="checkbox" v-model="newProfile.isBusiness">
-        </div>
-        <div class="profileCreationForm">
-            <h2>Nyelv: </h2>
-            <select v-model="selectedLanguage">
-                <option v-for="language in languages" v-bind:value="language.code">{{ language.name }}</option>
-            </select>
-        </div>
-        <div class="profileCreationForm">
-            <button @click="profileCreationToggle">Vissza</button>
-            <button @click="createNewProfile">Létrehozás</button>
-        </div>
+    <div @click="profileCreationToggle" class="new-profile profile-container"><span
+        class="material-symbols-outlined size-32">add</span></div>
+  </div>
+  <div v-if="profileCreation" class="profileCreation-container">
+    <div class="profileCreationForm">
+      <h2>Név: </h2>
+      <input type="text" v-model="newProfile.name">
     </div>
+    <div class="profileCreationForm">
+      <h2>Munkai-e: </h2>
+      <input type="checkbox" v-model="newProfile.isBusiness">
+    </div>
+    <div class="profileCreationForm">
+      <h2>Nyelv: </h2>
+      <select v-model="selectedLanguage">
+        <option v-for="language in languages" v-bind:value="language.code">{{ language.name }}</option>
+      </select>
+    </div>
+    <div class="profileCreationForm">
+      <h2>Pénznem: </h2>
+      <select v-model="selectedCurrency">
+        <option v-for="currency in currencies" v-bind:value="currency.code">{{ currency.name }}</option>
+      </select>
+    </div>
+    <div class="profileCreationForm">
+      <button @click="profileCreationToggle">Vissza</button>
+      <button @click="createNewProfile">Létrehozás</button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .profileCreation-container {
-    background-color: var(--sec-background);
-    width: 400px;
-    height: auto;
-    border-radius: var(--border-radius);
-    padding: 5px;
+  background-color: var(--sec-background);
+  width: 400px;
+  height: auto;
+  border-radius: var(--border-radius);
+  padding: 5px;
 
-    position: absolute;
-    top: 20%;
-    right: 50%;
-    transform: translateX(200px);
+  position: absolute;
+  top: 20%;
+  right: 50%;
+  transform: translateX(200px);
 }
 
-.profileCreationForm>* {
-    display: inline;
-    margin: 5px;
+.profileCreationForm > * {
+  display: inline;
+  margin: 5px;
 }
 
 .new-profile {
-    text-align: center;
-    align-items: center;
+  text-align: center;
+  align-items: center;
 }
 
 .profiles-container {
-    background-color: var(--sec-background);
-    width: 400px;
-    height: auto;
-    border-radius: var(--border-radius);
+  background-color: var(--sec-background);
+  width: 400px;
+  height: auto;
+  border-radius: var(--border-radius);
 
-    position: absolute;
-    top: 20%;
-    right: 50%;
-    transform: translateX(200px);
+  position: absolute;
+  top: 20%;
+  right: 50%;
+  transform: translateX(200px);
 }
 
 .profile-container {
-    padding: 5px;
-    margin: 5px;
-    border: 2px solid var(--main-background);
-    border-radius: var(--border-radius);
-    color: var(--sec-text-color);
-    cursor: pointer;
-    transition: all ease-out 0.2s;
+  padding: 5px;
+  margin: 5px;
+  border: 2px solid var(--main-background);
+  border-radius: var(--border-radius);
+  color: var(--sec-text-color);
+  cursor: pointer;
+  transition: all ease-out 0.2s;
 }
 
 .profile-container:hover {
-    background-color: var(--main-background);
-    color: var(--main-text-color);
-    transition: all ease-out 0.2s;
+  background-color: var(--main-background);
+  color: var(--main-text-color);
+  transition: all ease-out 0.2s;
 }
 </style>
